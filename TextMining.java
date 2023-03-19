f24d2f8fdb0b8acee321ff3dda8b1f38847e8948
Mina Pourostad <minasmacbook@minas-mbp.mynetworksettings.com>
2023-03-19 15:30:31 -0400

    using Div similarity and clustering class

--- TextMining.java
+++ TextMining.java
@@ -1,7 +1,11 @@
 
 import java.io.*;
-import java.util.*;public class TextMining{
-	public static void main(String[] args) throws IOException{
+import java.util.*;
+import java.util.stream.Collectors;
+import Jama.Matrix;
+import Jama.*;
+public class TextMining{
+	public static void main(String[] args) throws Exception{
 		DataCleaning dc = new DataCleaning();
 		List<File> inputFiles = new ArrayList<>();
 		String[] outputFileName = {"./dataset_3/data/C1_out/", "./dataset_3/data/C4_out/", "./dataset_3/data/C7_out/"};
@@ -18,10 +22,18 @@ import java.util.*;public class TextMining{
 		kmeansClustering clustering = new kmeansClustering(documentTermMatrix, 3);
 		clustering.setCosineSimilarity(true);
 		int[] clusters = clustering.cluster();
-		for (int c: clusters){
-			System.out.println(c);
+
+		double[][] reduced = reduceToTwoDimensions(documentTermMatrix);
+		printarray(reduced);
+		List<double[]> dataPoints = Arrays.stream(reduced)
+                            .map(row -> row.clone())
+                            .collect(Collectors.toList());
+        List<Integer> cluster = new ArrayList<>();
+		for (int c : clusters) {
+		    cluster.add(c);
 		}
-		
+		Visualization visualize = new Visualization();
+		visualize.visualize(dataPoints, cluster);
 		// for (String s: allTerms){
 		// 	System.out.print(s + " ");
 		// }
@@ -34,6 +46,27 @@ import java.util.*;public class TextMining{
 
 
 		System.exit(0);
+	}
+	public static double[][] reduceToTwoDimensions(double[][] matrix) {
+	    int numRows = matrix.length;
+	    int numCols = matrix[0].length;
+	    Matrix X = new Matrix(matrix);
+	    // Matrix X_centered = X.minus(X.mean(0));
+	    Matrix cov = X.transpose().times(X).times(1.0 / (numCols - 1));
+	    EigenvalueDecomposition ed = cov.eig();
+	    Matrix eigenvectors = ed.getV();
+	    Matrix X_pca = X.times(eigenvectors.getMatrix(0, numCols - 1, 0, 1));
+	    double[][] result = X_pca.getArray();
+	    return result;
+	}
+	public static void printarray(double [][] data){
+		for (int i = 0; i < data.length; i ++){
+			for (int j = 0; j < data[0].length; j++){
+				System.out.print(data[i][j]);
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
 	}
 }
 
--- visualization.java
+++ visualization.java
@@ -1,9 +1,8 @@
 import java.awt.Color;
-import java.awt.Dimension;
 import java.util.List;
-
+import java.util.Random;
 import org.jfree.chart.ChartFactory;
-import org.jfree.chart.ChartPanel;
+import org.jfree.chart.ChartFrame;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.chart.plot.XYPlot;
@@ -11,70 +10,90 @@ import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
 import org.jfree.data.xy.XYDataset;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
+import java.io.*;
+import org.jfree.chart.ChartUtilities;
 
-public class ClusterVisualizer {
+public class Visualization {
 
-    public void visualize(List<Document> documents, int[] clusterAssignments) {
-        // reduce dimensionality using PCA
-        double[][] pcaData = PCA.reduceDimensionality(documents);
+    // Method to visualize the clustering result
+    public void visualize(List<double[]> dataPoints, List<Integer> clusterIds) throws Exception {
+        // Generate random colors for each cluster
+        Color[] colors = generateColors(clusterIds.size());
 
-        // create chart dataset
-        XYDataset dataset = createDataset(pcaData, clusterAssignments);
+        // Create a dataset containing data points and their corresponding cluster ids
+        XYDataset dataset = createDataset(dataPoints, clusterIds);
 
-        // create chart
-        JFreeChart chart = ChartFactory.createScatterPlot("Document Clustering", "PCA Component 1",
-                "PCA Component 2", dataset, PlotOrientation.VERTICAL, true, true, false);
-
-        // customize chart
+        // Create a scatter plot with data points colored according to their clusters
+        JFreeChart chart = ChartFactory.createScatterPlot("Data Clustering Result", "X", "Y", dataset, PlotOrientation.VERTICAL, true, true, false);
         XYPlot plot = chart.getXYPlot();
-        plot.setBackgroundPaint(Color.WHITE);
-        plot.setDomainGridlinePaint(Color.BLACK);
-        plot.setRangeGridlinePaint(Color.BLACK);
-
-        // customize scatter plot renderer
-        XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer(false, true);
-        for (int i = 0; i < clusterAssignments.length; i++) {
-            renderer.setSeriesPaint(i, getClusterColor(clusterAssignments[i]));
-            renderer.setSeriesShapesVisible(i, true);
+        XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
+        for (int i = 0; i < clusterIds.size(); i++) {
+            renderer.setSeriesPaint(i, colors[clusterIds.get(i)]);
         }
         plot.setRenderer(renderer);
 
-        // display chart
-        ChartPanel chartPanel = new ChartPanel(chart);
-        chartPanel.setPreferredSize(new Dimension(500, 500));
-        JFrame frame = new JFrame("Document Clustering");
-        frame.setContentPane(chartPanel);
+        // Display the plot in a frame
+        ChartFrame frame = new ChartFrame("Data Clustering Result", chart);
         frame.pack();
         frame.setVisible(true);
+        int width = 640;   /* Width of the image */
+        int height = 480;  /* Height of the image */ 
+        File pieChart = new File( "Chart.jpeg" ); 
+        ChartUtilities.saveChartAsJPEG( pieChart , chart , width , height );
     }
 
-    private XYDataset createDataset(double[][] data, int[] clusterAssignments) {
+    // Method to generate random colors
+    private Color[] generateColors(int numColors) {
+        Color[] colors = new Color[numColors];
+        Random rand = new Random();
+        for (int i = 0; i < numColors; i++) {
+            colors[i] = new Color(rand.nextInt(256), rand.nextInt(256), rand.nextInt(256));
+        }
+        return colors;
+    }
+
+    // Method to create a dataset containing data points and their corresponding cluster ids
+    // private XYDataset createDataset(List<double[]> dataPoints, List<Integer> clusterIds) {
+    //     XYSeriesCollection dataset = new XYSeriesCollection();
+    //     // System.out.print(clusterIds);
+    //     for (int i = 0; i < clusterIds.size(); i++) {
+    //         // System.out.print(i);
+    //         int clusterId = clusterIds.get(i);
+    //         double[] dataPoint = dataPoints.get(i);
+    //         XYSeries series = dataset.getSeries(clusterId);
+    //         System.out.print("Thissssss");
+    //         if (series == null) {
+    //             series = new XYSeries("Cluster " + clusterId);
+    //             dataset.addSeries(series);
+    //         }
+    //         series.add(dataPoint[0], dataPoint[1]);
+    //     }
+    //     return dataset;
+    // }
+    private XYDataset createDataset(List<double[]> dataPoints, List<Integer> clusterIds) {
         XYSeriesCollection dataset = new XYSeriesCollection();
+        // System.out.print(clusterIds);
+        for (int i = 0; i < clusterIds.size(); i++) {
+            // System.out.print(i);
+            int clusterId = clusterIds.get(i);
+            double[] dataPoint = dataPoints.get(i);
 
-        // add data points to series
-        for (int i = 0; i < data.length; i++) {
-            XYSeries series = new XYSeries(i);
-            series.add(data[i][0], data[i][1]);
-            dataset.addSeries(series);
+            
+            // System.out.print("Thissssss");
+            try {
+              //  Block of code to try
+                XYSeries series = dataset.getSeries(clusterId);
+                series.add(dataPoint[0], dataPoint[1]);
+            }
+            catch(Exception e) {
+              //  Block of code to handle errors
+                XYSeries series = new XYSeries("Cluster " + clusterId);
+                dataset.addSeries(series);
+                series.add(dataPoint[0], dataPoint[1]);
+            }
+                
+            
         }
-
         return dataset;
-    }
-
-    private Color getClusterColor(int clusterAssignment) {
-        switch (clusterAssignment) {
-            case 0:
-                return Color.RED;
-            case 1:
-                return Color.BLUE;
-            case 2:
-                return Color.GREEN;
-            case 3:
-                return Color.ORANGE;
-            case 4:
-                return Color.MAGENTA;
-            default:
-                return Color.BLACK;
-        }
     }
 }
